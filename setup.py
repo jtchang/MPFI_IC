f070adf5e0f4421b7f6a2311bbf02b88479cc8df
Jeremy T. Chang <jeremy.chang@mpfi.org>
2019-02-27 22:17:13 -0500

    updates to setup.py with url and packages

--- setup.py
+++ setup.py
@@ -12,10 +12,10 @@ setup(
     description='A modular framework for the analysis of experiment data neuroscience experiments.',
     author='Jeremy T. Chang',
     author_email='jeremy.chang@mpfi.org',
-    url='https://github.com/jtchang/fleappy',
-    packages=['fleappy.notifications', 'fleappy.tiffread', 'fleappy.imgregistration'],
+    url='https://github.com/jtchang/MPFI_IC',
+    packages=['fleappy.notifications', 'fleappy.tiffread', 'fleappy.imgregistration', 'fleappy.experiment', 'fleappy.roimanager', 'fleappy.metadata', 'fleappy.imageviewer', 'fleappy.analysiis'],
     install_requires=required,
-    long_description='See ' + 'https://github.com/jtchang/fleappy',
+    long_description='See ' + 'https://mpif-ic.readthedocs.io',
     license='MIT',
     python_requires='~=3.7'
 )
